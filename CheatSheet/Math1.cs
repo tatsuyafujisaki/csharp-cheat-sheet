6ccde9ecd687cf355feb7d1773bc95140825d8a4
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2018-07-04 16:02:04 +0900

    Update Math1.cs

--- CheatSheet/Math1.cs
+++ CheatSheet/Math1.cs
@@ -8,22 +8,22 @@ namespace CheatSheet
         // e.g. Math.Round(2.5) returns 2 rather than 3.
         // e.g. Math.Round(2.05, 1) returns 2.0 rather than 2.1.
 
-        internal static decimal RoundToNearestWholeNumber(decimal x) => Math.Round(x, MidpointRounding.AwayFromZero);
-        internal static decimal Round(decimal x, int fractionalDigitCount) => Math.Round(x, fractionalDigitCount, MidpointRounding.AwayFromZero);
-        internal static string RoundAsString(decimal x, int fractionalDigitCount) => x.ToString("#,##0");
-        internal static string RoundToTwoDecimalPlacesAsString(decimal x, int fractionalDigitCount) => x.ToString("#,##0.##");
+        internal static decimal Round(decimal d) => Math.Round(d, MidpointRounding.AwayFromZero);
+        internal static decimal Round(decimal d, int fractionalDigitCount) => Math.Round(d, fractionalDigitCount, MidpointRounding.AwayFromZero);
+        internal static string RoundToString(decimal d, int fractionalDigitCount) => d.ToString("#,##0");
+        internal static string RoundToTwoDecimalPlacesToString(decimal d, int fractionalDigitCount) => d.ToString("#,##0.##");
 
         // Explanatory wrapper
-        internal static decimal RoundUpToNearestWholeNumber(decimal x) => Math.Ceiling(x);
+        internal static decimal RoundUp(decimal d) => Math.Ceiling(d);
 
-        internal static decimal RoundUp(decimal x, int fractionalDigitCount)
+        internal static decimal RoundUp(decimal d, int fractionalDigitCount)
         {
             var power = (decimal)Math.Pow(10, fractionalDigitCount);
-            return Math.Ceiling(x * power) / power;
+            return Math.Ceiling(d * power) / power;
         }
 
         // Explanatory wrapper
-        internal static decimal RoundDownToNearestWholeNumber(decimal x) => Math.Floor(x);
+        internal static decimal RoundDown(decimal x) => Math.Floor(x);
 
         internal static decimal RoundDown(decimal x, int fractionalDigitCount)
         {
